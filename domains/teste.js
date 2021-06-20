619caff89eb92e7257ab643f0375b40ccd04a662
Pedro Spoljaric <pedrospoljaric@gmail.com>
2021-06-20 13:28:55 -0300

    page test

--- domains/teste.js
+++ domains/teste.js
@@ -1,15 +1,7 @@
 /* eslint-disable no-param-reassign */
-const cheerio = require('cheerio')
-const request = require('request-promise')
 const puppeteer = require('puppeteer')
-const https = require('https')
-const FormData = require('form-data')
-const { prop } = require('lodash/fp')
-const { default: axios } = require('axios')
 
-const formUrlEncoded = (x) => Object.keys(x).reduce((p, c) => `${p}&${c}=${encodeURIComponent(x[c])}`, '')
-
-const teste = async ({ username = 'pedro.spoljaric', password = 'maria.maravilha123' }) => {
+module.exports = async ({ username = 'pedro.spoljaric', password = 'maria.maravilha123' }) => {
     const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
     page.goto('http://intranet.unifesp.br')
@@ -17,9 +9,5 @@ const teste = async ({ username = 'pedro.spoljaric', password = 'maria.maravilha
     await page.$eval('input[class=password]', (el) => { el.value = password })
     await page.click('input[type=submit]')
 
-    let stop
+    browser.close()
 }
-
-teste({}).catch((err) => {
-    let stop
-})
