5c59bc30b043bc79f6c136cceed2e2ce322bfe4b
Pedro Spoljaric <pedrospoljaric@gmail.com>
2021-06-22 01:04:13 -0300

    heroku test

--- database/knexfile.js
+++ database/knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
     client: 'pg',
     connection: {
         host: process.env.DB_HOST,
-        database: process.env.DATABASE,
+        database: process.env.DB_NAME,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD
     },
