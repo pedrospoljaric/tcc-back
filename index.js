ec2904bf957004f2a7df4a85051c14cee03f8402
Pedro Spoljaric <pedrospoljaric@gmail.com>
2021-06-26 14:29:50 -0300

    store async error and try to allow cors better

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const routes = require('#routes')
 const app = express()
 const PORT = process.env.PORT || 1234
 
-app.use(cors({
-    origin: true
-}))
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
