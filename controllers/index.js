82dae1f6c70f81906d00370a47405c1fd7ec74bb
Pedro Spoljaric <pedrospoljaric@gmail.com>
2021-06-26 14:22:56 -0300

    store async error and try to allow cors better

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ module.exports = (controllerMethod) => async (req, res, next) => {
 
     try {
         const data = await domainMethod(parameters)
+        res.header('Access-Control-Allow-Origin', '*')
         return res.json(200, {
             success: true,
             data
