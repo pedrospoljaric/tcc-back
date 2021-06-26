c111776c27925c80ed25deefd60085854110e23b
Pedro Spoljaric <pedrospoljaric@gmail.com>
2021-06-26 16:36:23 -0300

    try to fix cors

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 1234
 
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(cors({ preflightContinue: true }))
-app.options(cors({ preflightContinue: true }))
 
 app.use('/api', routes)
 
