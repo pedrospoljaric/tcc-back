0ef0d3ec92203c6d979a4d040eabc59abc1ab038
Pedro Spoljaric <pedrospoljaric@gmail.com>
2021-09-18 19:18:50 -0300

    configure vercel

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@ const app = require('./router')()
 const health = require('./health')
 
 app
-    .get('/api/health', health)
+    .get('/api/health', (req, res) => res.send('sim'))
 
 module.exports = app
